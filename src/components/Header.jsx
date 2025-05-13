9fcccf3bb9398f54e705cd6242a276ab611dd376
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-13 16:22:05 +0100

    icon and title amended. Nav added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 export default function Header() {
     return (
         <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
-        <h1 className="text-2xl font-josefin font-bold">Movie Reviews</h1>
+            <h1 className="text-2xl font-josefin font-bold">Movie Reviews</h1>
+            <nav className="flex space-x-4">
+                <a href="#" className="hover:text-gray-400">Home</a>
+                <a href="#" className="hover:text-gray-400">About</a>
+                <a href="#" className="hover:text-gray-400">Contact</a>
+            </nav>
         </header>
     )
 }
