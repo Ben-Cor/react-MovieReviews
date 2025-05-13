2af5c127604840663207373316ae73cb10bc3fe3
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-13 16:24:04 +0100

    nav amended

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ export default function Header() {
         <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
             <h1 className="text-2xl font-josefin font-bold">Movie Reviews</h1>
             <nav className="flex space-x-4">
-                <a href="#" className="hover:text-gray-400">Home</a>
-                <a href="#" className="hover:text-gray-400">About</a>
-                <a href="#" className="hover:text-gray-400">Contact</a>
+                <a href="#" className="hover:text-gray-400">Top Rated Films</a>
+                <a href="#" className="hover:text-gray-400">Lowest Rated Films</a>
+                <a href="#" className="hover:text-gray-400">Most Popular Films</a>
             </nav>
         </header>
     )
