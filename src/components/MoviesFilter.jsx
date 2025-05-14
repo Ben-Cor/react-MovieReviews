bfa7a069c29988de3c75ac2272c9db8b773cecd9
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-14 10:13:53 +0100

    added rating filter functionality

--- src/components/MoviesFilter.jsx
+++ src/components/MoviesFilter.jsx
@@ -3,14 +3,29 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesFilter() {
     const [films, setFilms] = useState([]); // State to store fetched films
+    const [rating, setRating] = useState(0); // State to store the selected rating
+    const [filteredFilms , setFilteredFilms] = useState([]); // State to store filtered films
+
 
     const fetchFilms = async () => {
         try {
             const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e8c2e0cec179f15c034b52f686d8ce61");
             const data = await response.json();
             setFilms(data.results); // Store the fetched films in state
+            setFilteredFilms(data.results); // Initialize filtered films with all fetched films
         } catch (error) {
             console.error("Error fetching films:", error);
+        }
+    }
+
+    const handleClickRating = (ratingFilter) => {
+        if (ratingFilter === rating) {
+            setRating(0); // Reset rating if the same button is clicked
+            setFilms(filteredFilms); // Show all films
+        } else {
+            setRating(ratingFilter); // Set the new rating
+            const filtered = filteredFilms.filter((film) => film.vote_average >= ratingFilter); // Filter films based on rating
+            setFilms(filtered);
         }
     }
 
@@ -23,9 +38,9 @@ export default function MoviesFilter() {
                 <h2 className="text-2xl">Popular</h2>
                 <div className="flex items-center gap-8">
                     <ul className="flex space-x-4">
-                        <li>8+</li>
-                        <li>7+</li>
-                        <li>6+</li>
+                        <li className="cursor-pointer" onClick={() => handleClickRating(8)}>8+</li>
+                        <li className="cursor-pointer" onClick={() => handleClickRating(7)}>7+</li>
+                        <li className="cursor-pointer" onClick={() => handleClickRating(6)}>6+</li>
                     </ul>
                     <div className="flex gap-8">
                         <select className="border-[1px] rounded-sm p-2" name="filter" id="filter">
